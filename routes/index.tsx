fd4210b34bbe08fb4ed6ff214c136f23017c9480
Ryan Dahl <ry@tinyclouds.org>
2023-06-20 16:26:16 +0200

    index.html style

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,15 +3,22 @@ import Form from "../islands/Form.tsx";
 
 export default function Home() {
   return (
-    <>
+    <div class="bg-blue-200 min-h-screen flex items-center justify-center">
       <Head>
         <title>Sea-Mail</title>
       </Head>
-      <img class="max-w-md mx-auto my-8" src="pirate.webp" />
-      <h1 class="max-w-md mx-auto my-8 text-3xl text-center font-bold leading-7">
-        Sea Mail
-      </h1>
-      <Form />
-    </>
+      <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+        <div class="mb-4 text-center">
+          <h1 class="text-2xl">Sea Mail</h1>
+          <img src="pirate.webp" alt="Pirate Logo" class="mx-auto w-64" />
+          <p class="mt-2 text-grey-darker text-sm">
+            Welcome to Sea Mail! Submit an email and message, and our trusty
+            pirate will translate your message into pirate-speak and send it on
+            your behalf. Ahoy!
+          </p>
+        </div>
+        <Form />
+      </div>
+    </div>
   );
 }
