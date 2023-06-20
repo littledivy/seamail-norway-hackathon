9826ce3406b84f4d6cc01e74631cee98ad411a1d
Ryan Dahl <ry@tinyclouds.org>
2023-06-20 12:16:10 +0200

    add logo

--- islands/Form.tsx
+++ islands/Form.tsx
@@ -1,43 +1,44 @@
 export default function Submit() {
-    const handleSubmit = async () => {
-      const resp = await fetch("/api/openai", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-    };
+  const handleSubmit = async () => {
+    const resp = await fetch("/api/openai", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
 
-    return (
-        <div>
-        <div className="container mx-auto p-8 bg-gray-100 rounded-md max-w-md">
-          <div className="flex flex-col">
-            <label htmlFor="email" className="mb-2 font-bold text-gray-700">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="px-4 py-2 border border-gray-300 rounded-md bg-white text-gray-700"
-              required
-            />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="message" className="mb-2 font-bold text-gray-700">
-              Message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              rows="4"
-              className="px-4 py-2 border border-gray-300 rounded-md bg-white text-gray-700"
-              required
-            >
-            </textarea>
-          </div>
+  return (
+    <div>
+      <div className="container mx-auto p-8 bg-gray-100 rounded-md max-w-md">
+        <div className="flex flex-col">
+          <label htmlFor="email" className="mb-2 font-bold text-gray-700">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="px-4 py-2 border border-gray-300 rounded-md bg-white text-gray-700"
+            required
+          />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="message" className="mb-2 font-bold text-gray-700">
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            rows="4"
+            className="px-4 py-2 border border-gray-300 rounded-md bg-white text-gray-700"
+            required
+          >
+          </textarea>
+        </div>
 
-          {/* <div className="flex flex-col">
+        {
+          /* <div className="flex flex-col">
             <label
               htmlFor="messageType"
               className="mb-2 font-bold text-gray-700"
@@ -56,17 +57,18 @@ export default function Submit() {
               <option value="robot">Robot</option>
               <option value="rapper">Rapper</option>
             </select>
-          </div> */}
-          <div className="flex justify-center">
-            <button
-              type="button"
-              onClick={() => handleSubmit()}
-              className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
-            >
-              Send
-            </button>
-          </div>
+          </div> */
+        }
+        <div className="flex justify-center">
+          <button
+            type="button"
+            onClick={() => handleSubmit()}
+            className="p-4 py-2 my-8 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+          >
+            Send
+          </button>
         </div>
       </div>
-    );
+    </div>
+  );
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -5,8 +5,12 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Seamail</title>
+        <title>Sea-Mail</title>
       </Head>
+      <img class="max-w-md mx-auto my-8" src="pirate.webp" />
+      <h1 class="max-w-md mx-auto my-8 text-3xl text-center font-bold leading-7">
+        Sea Mail
+      </h1>
       <Form />
     </>
   );
